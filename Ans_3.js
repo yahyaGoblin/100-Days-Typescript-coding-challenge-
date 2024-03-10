1a6fce780dfef7de25b4ab893f123185fdb34ccc
yahyaGoblin <122987586+yahyaGoblin@users.noreply.github.com>
2024-03-10 21:20:30 +0500

    Add files via upload

---
+++ Ans_3.js
@@ -0,0 +1,4 @@
+var Name = "Yahya"; // This saves the name
+console.log(Name.toLowerCase()); // lowercase
+console.log(Name.toUpperCase()); // upercase
+console.log(Name.charAt(0).toUpperCase() + Name.slice(1).toLowerCase()); // Puctual writing
