572037093d7098caad25d27a9c8e61aac6d3a79d
yahyaGoblin <122987586+yahyaGoblin@users.noreply.github.com>
2024-04-06 20:36:15 +0500

    Updated Repo

---
+++ day-3/Ans-9/main.js
@@ -0,0 +1,2 @@
+var favoriteNumber = 17; // saves favorite number
+console.log("My favorite number is ".concat(favoriteNumber, ".")); // shows message with favorite number
---
+++ day-3/Ans-9/main.ts
@@ -0,0 +1,2 @@
+let favoriteNumber: number = 17; // saves favorite number
+console.log(`My favorite number is ${favoriteNumber}.`); // shows message with favorite number
