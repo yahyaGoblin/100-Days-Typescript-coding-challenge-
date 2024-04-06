315c794b48bc6e2e36b5a446fb71953ff4836b73
yahyaGoblin <122987586+yahyaGoblin@users.noreply.github.com>
2024-04-06 20:34:29 +0500

    Add files via upload

---
+++ day-2/Ans-6/main.js
@@ -0,0 +1,3 @@
+var myname = "\t\n Yahya \t\n"; // This saves the name with whitespace
+console.log(myname); // Shows the name with whitespace
+console.log(myname.trim()); // Shows the name without whitespace
---
+++ day-2/Ans-6/main.ts
@@ -0,0 +1,4 @@
+let myname: string = "\t\n Yahya \t\n"; // This saves the name with whitespace
+
+console.log(myname); // Shows the name with whitespace
+console.log(myname.trim()); // Shows the name without whitespace
