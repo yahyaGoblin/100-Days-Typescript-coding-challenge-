aad929b2c10b09d2f15a618e44a9ce53ae4af4b0
yahyaGoblin <122987586+yahyaGoblin@users.noreply.github.com>
2024-04-06 20:39:00 +0500

    Updated Repo

---
+++ day-5/Ans-14/main.js
@@ -0,0 +1,4 @@
+var guests = ["Albert Einstein", "Issac Newton", "Nikola Tesla"];
+guests.forEach(function (guest) {
+    console.log("Dear ".concat(guest, ", would you like to join me for dinner?"));
+});
---
+++ day-5/Ans-14/main.ts
@@ -0,0 +1,6 @@
+let guests: string[] = ["Albert Einstein", "Issac Newton", "Nikola Tesla"];
+
+guests.forEach(guest => 
+    {
+        console.log(`Dear ${guest}, would you like to join me for dinner?`);
+    });
