de670f9d9b69e14615ed1e90157339abe3dc0e85
yahyaGoblin <122987586+yahyaGoblin@users.noreply.github.com>
2024-04-06 20:37:24 +0500

    Updated Repo

---
+++ day-4/Ans-11/main.js
@@ -0,0 +1,4 @@
+var names = ["Kim", "Charlie", "Karen"];
+for (var i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
---
+++ day-4/Ans-11/main.ts
@@ -0,0 +1,4 @@
+let names: string[] = ["Kim", "Charlie", "Karen"];
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
