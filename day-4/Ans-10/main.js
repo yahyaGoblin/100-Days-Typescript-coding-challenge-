a55559754219eb341f5f91e51b1fec421218060b
yahyaGoblin <122987586+yahyaGoblin@users.noreply.github.com>
2024-04-06 20:37:00 +0500

    Updated Repo

---
+++ day-4/Ans-10/main.js
@@ -0,0 +1,4 @@
+// Yahya, 2024-04-05
+// This program prints a personal message.
+var myName = "Yahya";
+console.log("Hello ".concat(myName, ", would you like to learn some TypeScript today?"));
---
+++ day-4/Ans-10/main.ts
@@ -0,0 +1,4 @@
+// Yahya, 2024-04-05
+// This program prints a personal message.
+let myName: string = "Yahya";
+console.log(`Hello ${myName}, would you like to learn some TypeScript today?`);
