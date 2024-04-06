7ca7d9c091ba1c9f058cd76bb3c9c10c579f256f
yahyaGoblin <122987586+yahyaGoblin@users.noreply.github.com>
2024-04-06 20:38:32 +0500

    Updated Repo

---
+++ day-5/Ans-13/main.js
@@ -0,0 +1,4 @@
+var transports = ["Kawasaki Ninja H2", "Tesla Roadster", "AH-64 Apache", "koenigsegg agera r"];
+transports.forEach(function (transport) {
+    console.log("I would like to own a ".concat(transport, "."));
+});
---
+++ day-5/Ans-13/main.ts
@@ -0,0 +1,4 @@
+let transports: string[] = ["Kawasaki Ninja H2", "Tesla Roadster", "AH-64 Apache", "koenigsegg agera r"];
+transports.forEach(transport => {
+    console.log(`I would like to own a ${transport}.`);
+});
